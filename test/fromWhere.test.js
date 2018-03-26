b97b34573c185efa379c1d690330e7107326a3a1
siphenkosi salman <32076844+siphera@users.noreply.github.com>
2018-03-26 16:06:53 +0200

    Update fromWhere.test.js

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -1,14 +1,14 @@
 describe('fromWhere' ,function(){
 
   it('should return "Bellville" if the registration number starts Wwith CY', function(){
-      assert.equal(fromWhere("CY"),"Bellville");
+      assert.equal(fromWhere("CY 9879"),"Bellville");
   });
 
   it('should return "Paarl" if registration number startsWith (CJ)', function(){
-      assert.equal(fromWhere("CJ"),"Paarl");
+      assert.equal(fromWhere("CJ 9796"),"Paarl");
   });
   it('should return "Cape Town" if registration number startsWith (CA)', function(){
-      assert.equal(fromWhere("CA"),"Cape Town");
+      assert.equal(fromWhere("CA 7865"),"Cape Town");
   });
 
   it('should return "Some other place!"" its from any other place', function(){
